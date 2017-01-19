1f67e43877bac52d73f691fed4958e43b8529a4d
suhao <valar_su@163.com>
2017-01-19 23:52:37 +0800

    bidn

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/test', function (req, res, next) {
+    res.render('test', {
+      title: '测试测试测试'
+    });
+});
+
 module.exports = router;
